f0b4cf2585077a4381cf69fc183eec87c5e41a28
sdellatorre <sdellatorre@gmail.com>
2018-04-26 19:16:59 -0300

    Enlace

--- src/edu/udc/lapii/mvc/tateti/JugarTatetiSwing.java
+++ src/edu/udc/lapii/mvc/tateti/JugarTatetiSwing.java
@@ -5,10 +5,11 @@ import edu.udc.lapii.mvc.tateti.core.TatetiController;
 import edu.udc.lapii.mvc.tateti.swing.TatetiFrame;
 
 public class JugarTatetiSwing {
-	
+	/* https://youtu.be/7XUKVj328z0
+	 */
 	public static void main(String[] args) {
 		Tateti model = new Tateti();
 		new TatetiController(model, new TatetiFrame(model));
 	}
-
+	
 }
